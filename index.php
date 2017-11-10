d5397a1a952ad9f84172e097432dab2f9980ff93
ThomasJones69 <tjones@ziggo.nl>
2017-11-10 14:42:06 +0100

    Nu komen de images in beeld

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 $dieren = array("Bear", "Bear", "Elephant", "Elephant", "Lion", "Lion", "Panda", "Panda", "Tiger", "Tiger", "Turtle", "Turtle", "Wolf", "Wolf", "Zebra", "Zebra");
-for ($y = 1; $y < count($dieren); $y++) {
+for ($y = 0; $y < count($dieren); $y++) {
     //               $id = $x . $y;
     $tegel = $dieren[$y];
-    echo alert($tegel);
-    echo $dieren["<img src='$tegel.jpg'>"];
+
+ //   echo $dieren["<img src='$tegel.jpg'>"];
+    echo "<img src=$dieren[$y].jpg>";
 }
 ?>
